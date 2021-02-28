d7ef8c071f008afec8c34bc07978dc40fd98a8fe
Diego <diegofytn@gmail.com>
2021-02-27 22:42:27 -0600

    Ejercicios desafío

---
+++ desafio_practico1/app/src/main/java/sv/edu/udb/desafio_practico1/Ejercicio2.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.desafio_practico1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio2);
+    }
+}
---
+++ desafio_practico1/app/src/main/java/sv/edu/udb/desafio_practico1/Result_ejercicio3.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.desafio_practico1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result_ejercicio3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_ejercicio3);
+    }
+}
